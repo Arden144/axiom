d97f51c4a50277d8200b53000b0e0178b3923ddf
Arden <general@ardensinclair.com>
2022-06-26 23:50:30 -0600

    Fix wrong function for adding field to embed

--- embeds/play.go
+++ embeds/play.go
@@ -12,8 +12,8 @@ func Play(track lavalink.AudioTrackInfo) discord.Embed {
 	embed.SetAuthorName("Now playing")
 	embed.SetTitle(track.Title)
 	embed.SetURL(*track.URI)
-	embed.SetField(0, "Channel", track.Author, true)
-	embed.SetField(1, "Duration", track.Length.String(), true)
+	embed.AddField("Channel", track.Author, true)
+	embed.AddField("Duration", track.Length.String(), true)
 	embed.SetThumbnail(fmt.Sprintf("https://img.youtube.com/vi/%v/default.jpg", track.Identifier))
 	return embed.Build()
 }
