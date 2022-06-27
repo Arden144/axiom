e6cf1e8a578fcd1d44335c67dd7e2713adda5153
Arden <general@ardensinclair.com>
2022-06-26 23:46:59 -0600

    Update play embed to match Axius

--- embeds/play.go
+++ embeds/play.go
@@ -9,14 +9,11 @@ import (
 
 func Play(track lavalink.AudioTrackInfo) discord.Embed {
 	embed := discord.NewEmbedBuilder()
+	embed.SetAuthorName("Now playing")
 	embed.SetTitle(track.Title)
+	embed.SetURL(*track.URI)
+	embed.SetField(0, "Channel", track.Author, true)
+	embed.SetField(1, "Duration", track.Length.String(), true)
 	embed.SetThumbnail(fmt.Sprintf("https://img.youtube.com/vi/%v/default.jpg", track.Identifier))
-	embed.SetFields(discord.EmbedField{
-		Name:  "Channel",
-		Value: track.Author,
-	}, discord.EmbedField{
-		Name:  "Duration",
-		Value: track.Length.String(),
-	})
 	return embed.Build()
 }
