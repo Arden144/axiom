aadd37a91b7f654029b206f08f6e28448d806b01
Arden <general@ardensinclair.com>
2022-07-04 15:23:02 -0600

    Increase embed image quality

--- embeds/error.go
+++ embeds/error.go
@@ -9,7 +9,7 @@ func Error() discord.Embed {
 	embed := discord.NewEmbedBuilder()
 	embed.SetColor(color.Red)
 	embed.SetAuthorName("Failed")
-	embed.SetAuthorIcon("https://github.com/material-icons/material-icons-png/raw/master/png/white/error_outline/baseline.png")
+	embed.SetAuthorIcon("https://github.com/material-icons/material-icons-png/raw/master/png/white/error_outline/baseline-4x.png")
 	embed.SetTitle("Unfortunately, an unexpected error has occured while trying to process your command.")
 	return embed.Build()
 }
--- embeds/play.go
+++ embeds/play.go
@@ -12,11 +12,11 @@ func Play(track lavalink.AudioTrackInfo) discord.Embed {
 	embed := discord.NewEmbedBuilder()
 	embed.SetColor(color.Blue)
 	embed.SetAuthorName("Now Playing")
-	embed.SetAuthorIcon("https://github.com/material-icons/material-icons-png/raw/master/png/white/play_circle_outline/baseline.png")
+	embed.SetAuthorIcon("https://github.com/material-icons/material-icons-png/raw/master/png/white/play_circle_outline/baseline-4x.png")
 	embed.SetTitle(track.Title)
 	embed.SetURL(*track.URI)
 	embed.AddField("Channel", track.Author, true)
 	embed.AddField("Duration", track.Length.String(), true)
-	embed.SetThumbnail(fmt.Sprintf("https://img.youtube.com/vi/%v/default.jpg", track.Identifier))
+	embed.SetThumbnail(fmt.Sprintf("https://img.youtube.com/vi/%v/mqdefault.jpg", track.Identifier))
 	return embed.Build()
 }
