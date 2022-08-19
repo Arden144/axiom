688e1642d3748efbaff389f8b9473d5073580c1d
Arden <general@ardensinclair.com>
2022-08-19 04:50:42 -0600

    Fix incorrect time till play on queued track

--- commands/play.go
+++ commands/play.go
@@ -63,9 +63,8 @@ var Play = bot.Command{
 		}
 
 		if player.Playing() {
-			player.Enqueue(track)
-			msg.SetContent(fmt.Sprint("Queued ", track.Info().Title))
 			length := player.PlayingTrack().Info().Length - player.Position() + player.Remaining()
+			player.Enqueue(track)
 			msg.SetEmbeds(embeds.Queue(track.Info(), length))
 		} else {
 			if err := player.Play(track); err != nil {
